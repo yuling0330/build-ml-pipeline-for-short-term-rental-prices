cf42565045749f43264c841da24da157cc0f56ea
yulinggu0330 <yuling.gu@zebra.com>
2023-10-04 15:12:48 -0500

    adding test cases to check data

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    check if the test data has correct number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    check if the data is within price range
+    """
+    assert data['price'].between(min_price, max_price).all()
